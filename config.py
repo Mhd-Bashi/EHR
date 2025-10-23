414b8922c2e7a474bc582e92cfb84bf30f897e6d
mhd-bashi <muhammad.otahbashi@altruan.de>
2025-10-23 12:47:48 +0200

    feat: Update config to use environment variables for sensitive information

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@localhost/ehr_system"
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = "f8d88ec30c2e009dce1f76c91b0be88d458eb73dc152523f6258b8ba68b108ee"
-    SECURITY_PASSWORD_SALT = "619cce89f6d4a67b485eb2b787016d2c"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECURITY_PASSWORD_SALT = os.getenv("SECURITY_PASSWORD_SALT")
 
     # SMTP config (example: Gmail – for dev/testing use an app password)
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = "u2302900270@gmail.com"
-    MAIL_PASSWORD = "swkc vdsl vyjh fdug"
-    MAIL_DEFAULT_SENDER = ("EHR System", "u2302900270@gmail.com")
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT")
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
